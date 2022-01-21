d902989310f20ba7e674d1502d3389f1aca8547c
devgcard@gmail.com <devgcard@gmail.com>
2022-01-21 19:24:54 +0000

    Questionare now contains a ButtonGroup and Button

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -1,16 +1,29 @@
+import { Button, ButtonGroup } from "react-bootstrap";
 import Question from "./Question";
 
 const Questionare = (props) => {
-    
+
+    const allMarked = false
+
     console.log(props.questions)
-    
-    return (  
-        <div>
-            {props.questions.map(question => <Question question={question} handleCheck={props.handleCheck}/>)}
-            <button onClick={props.handleSubmit}>Submit Answers</button>
-            <button onClick={props.handleClear}>Clear Answers</button>
+
+    return (
+        <div style={{ width: '45vw', margin: 'auto auto' }} className="mt-4">
+            {props.questions.map(question => <Question question={question} handleClick={props.handleClick} />)}
+
+            <ButtonGroup className='d-flex justify-content-center' >
+                <Button
+                    variant='success'
+                    onClick={props.handleSubmit}
+                    disabled={!allMarked}
+                >Submit Answers</Button>
+                <Button
+                    variant='danger'
+                    onClick={props.handleClear}
+                >Clear Answers</Button>
+            </ButtonGroup>
         </div>
     );
 }
- 
+
 export default Questionare;
