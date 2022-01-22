ae7d35b5d5af6d90ba88bd70b1baf07db1e8e634
devgcard@gmail.com <devgcard@gmail.com>
2022-01-22 17:19:45 +0000

    Cleaned some lines

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -2,15 +2,11 @@ import { Button, ButtonGroup } from "react-bootstrap";
 import Question from "./Question";
 
 const Questionare = (props) => {
-
     const allMarked = false
-
-    console.log(props.questions)
 
     return (
         <div style={{ width: '45vw', margin: 'auto auto' }} className="mt-4">
             {props.questions.map(question => <Question question={question} handleClick={props.handleClick} />)}
-
             <ButtonGroup className='d-flex justify-content-center' >
                 <Button
                     variant='success'
