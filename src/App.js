ec4e8b4a4a1db76f45881f5853b78914459545d1
devgcard@gmail.com <devgcard@gmail.com>
2022-01-22 17:17:40 +0000

    quizOptions added quizUrl added added endGame function handleClick updated moved shuffleArray from Question to App

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ import { nanoid } from "nanoid";
 function App() {
   const [isRunning, setIsRunning] = React.useState(false)
   const [questions, setQuestions] = React.useState({})
+  const [indexOfSelectedAnswers, setIndexOfSelectedAnswers] = React.useState()
+
+  const quizOptions = {
+    numberOfQuestions: 5,
+    category: 9,
+    difficulty: '',
+    type: '',
+    encoding: '',
+  }
+
+  const quizUrl = `https://opentdb.com/api.php?amount=${quizOptions.numberOfQuestions ? `${quizOptions.numberOfQuestions}` : `1`}${quizOptions.category ? `&category=${quizOptions.category}` : ``}${quizOptions.difficulty ? `&difficulty=${quizOptions.difficulty}` : ``}${quizOptions.type ? `&type=${quizOptions.type}` : ``}`
+
 
   React.useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
+    console.log(quizUrl)
+    fetch(quizUrl)
       .then(res => res.json())
-      .then(data => setQuestions(data.results.map(item =>
-      (
-        {
-          ...item,
-          id: nanoid()
+      .then(data => setQuestions(data.results.map(item => {
+        const { correct_answer, incorrect_answers, type, question } = item
+        const answers = [...incorrect_answers, correct_answer]
+
+        return {
+          id: nanoid(),
+          question: question,
+          type: type,
+          answers: answers.map((answer, i) => ({
+            id: nanoid(),
+            answer: answer,
+            isCorrect: i === answers.length - 1 ? true : false,
+            selected: false
+          })),
         }
-      )
+      }
       )))
-
-
     console.log(questions)
   }, [!isRunning])
 
@@ -36,8 +56,32 @@ function App() {
     console.log('answers cleared!')
   }
 
-  function handleClick(event, id) {
-    console.log(`${id} clicked`)
+  function endGame() {
+    console.log('game ended')
+  }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function handleClick(event, answerId, questionId) {
+    console.clear()
+    // console.log(`id: ${answerId}`)
+    // console.log(`question: ${questionId}`)
+    const index = questions.findIndex(question => question.id === questionId)
+    // console.log(`index of question in questions array: ${index}`)
+    const { isCorrect } = questions[index].answers.find(answer => answer.id === answerId)
+    // console.log(`is this answer correct?\n${isCorrect}`)
+
+    setIndexOfSelectedAnswers(prev => ({
+      ...prev,
+      [questionId]: answerId
+    }))
+
+    console.log(indexOfSelectedAnswers)
   }
 
   return (
