40c13bae6f0fa970bda635bfc001bbec19e9a70f
devgcard@gmail.com <devgcard@gmail.com>
2022-01-23 16:19:01 +0000

    Removed allMarked Questionare receives maySubmit prop that does same job Question receives key prop

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -1,17 +1,17 @@
+import { nanoid } from "nanoid";
 import { Button, ButtonGroup } from "react-bootstrap";
 import Question from "./Question";
 
 const Questionare = (props) => {
-    const allMarked = false
 
     return (
         <div style={{ width: '45vw', margin: 'auto auto' }} className="mt-4">
-            {props.questions.map(question => <Question question={question} handleClick={props.handleClick} />)}
+            {props.questions.map(question => <Question question={question} handleClick={props.handleClick} key={nanoid()} />)}
             <ButtonGroup className='d-flex justify-content-center' >
                 <Button
                     variant='success'
                     onClick={props.handleSubmit}
-                    disabled={!allMarked}
+                    disabled={!props.maySubmit}
                 >Submit Answers</Button>
                 <Button
                     variant='danger'
