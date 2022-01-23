7dbf4843bddef8e383a2118a0211b03b54934290
devgcard@gmail.com <devgcard@gmail.com>
2022-01-23 16:11:59 +0000

    Changed Button to Form.Check Answers are now radio buttons with Label only

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,16 +1,27 @@
-import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import React from 'react'
+import { nanoid } from "nanoid";
 const Answer = (props) => {
 
     return (
-        <Button
-            variant={props.clicked ? 'primary' : 'secondary'}
-            onClick={(event) => props.handleClick(event, props.answerId, props.questionId)}
-            className={`border ${props.clicked ? 'border-secondary' : 'border-dark'} border-3 rounded`}
+        <div>
+            <Form.Check
+                id={`${props.answerId}-radio`}
+                variant={props.clicked ? 'primary' : 'secondary'}
+            >
+                <Form.Check.Label className="btn btn-primary">
+                    {props.answer}
+                    <Form.Check.Input
+                        style={{ visibility: 'hidden' }}
+                        name={`${props.question}-group`}
+                        type={'radio'}
+                        onClick={(event) => props.handleClick(event, props.answerId, props.questionId)}
+                    />
+                </Form.Check.Label>
+            </Form.Check>
 
-        >
-            {props.answer}
-        </Button>
+        </div>
+
     )
 }
 
