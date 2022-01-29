2b9e830297c74fb8cd83f77f8709e4ee8e995276
devgcard@gmail.com <devgcard@gmail.com>
2022-01-29 21:24:10 +0000

    New import Renamed prop New conditional rendering at :20

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -1,12 +1,23 @@
 import { nanoid } from "nanoid";
-import { Button, ButtonGroup } from "react-bootstrap";
+import { Button, ButtonGroup, Alert} from "react-bootstrap";
 import Question from "./Question";
 
 const Questionare = (props) => {
+    
+    const scoreAlert = (
+        <Alert className='mt-1'>
+            <Alert.Heading>
+                Quiz Complete!
+            </Alert.Heading>
+            <hr />
+            <p>You have correctly choosen <b>{}</b> out of <b>{props.questions.length}</b> answers</p>
+            <p>Meaning, you got <b>55%</b> of the quiz right!</p>
+        </Alert>
+    )
 
     return (
         <div style={{ width: '45vw', margin: 'auto auto' }} className="mt-4">
-            {props.questions.map(question => <Question question={question} handleClick={props.handleClick} key={nanoid()} />)}
+            {props.questions.map(question => <Question questionData={question} handleClick={props.handleClick} key={nanoid()} />)}
             <ButtonGroup className='d-flex justify-content-center' >
                 <Button
                     variant='success'
@@ -18,6 +29,7 @@ const Questionare = (props) => {
                     onClick={props.handleClear}
                 >Clear Answers</Button>
             </ButtonGroup>
+            {props.displayAlert.current && scoreAlert}
         </div>
     );
 }
