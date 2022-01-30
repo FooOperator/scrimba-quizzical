9c0534e192baac7dc82b9f431c6155f5b5da2a65
devgcard@gmail.com <devgcard@gmail.com>
2022-01-30 19:39:09 +0000

    Props has been destructured

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -2,20 +2,30 @@ import { Form } from "react-bootstrap";
 import React from 'react'
 import { nanoid } from "nanoid";
 
-const Answer = (props) => {
+const Answer = ({answerData, disabled, questionData, handleClick}) => {
+    const getClassNameForAnswer = () => {
+        const className = 'btn '
+        if (disabled) {
+            return answerData.isCorrect ? className + 'btn-success' : className +  'btn-danger'
+        }
+        return className + 'btn-primary'
+    }
+    
+
     return (
         <div>
             <Form.Check
-                id={`${props.answerId}-radio`}
-                variant={props.clicked ? 'primary' : 'secondary'}
+                id={`${answerData.answer}-radio`}
+
             >
-                <Form.Check.Label className="btn btn-primary">
-                    {props.answer}
+                <Form.Check.Label className={getClassNameForAnswer()}>
+                    {answerData.answer}
                     <Form.Check.Input
                         style={{ visibility: 'hidden' }}
-                        name={`${props.question}-group`}
+                        name={`${questionData.question}-group`}
                         type={'radio'}
-                        onClick={(event) => props.handleClick(event, props.answerId, props.questionId)}
+                        onClick={(event) => handleClick(event, answerData.id, questionData.id)}
+                        disabled={disabled}
                     />
                 </Form.Check.Label>
             </Form.Check>
