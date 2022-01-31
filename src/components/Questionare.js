5e09b3e09eea7fb3a7f9df18795268da662765ba
devgcard@gmail.com <devgcard@gmail.com>
2022-01-31 04:03:53 +0000

    Implemented scrollToRef functionality aesthetic changes, style changes conditional rendering at :51 -> :72

--- src/components/Questionare.js
+++ src/components/Questionare.js
@@ -5,38 +5,71 @@ import Question from "./Question";
 const Questionare = (props) => {
     const correctPercentage = parseInt((props.score * 100) / props.questions.length)
     const scoreAlert = (
-        <Alert className='mt-2'>
-            <Alert.Heading>
-                Quiz Complete!
-            </Alert.Heading>
-            <hr />
-            <p>You have correctly choosen <b>{props.score}</b> out of <b>{props.questions.length}</b> answers</p>
-            <p>Meaning, you got <b>{correctPercentage}%</b> of the quiz right!</p>
-        </Alert>
+        <>
+            <Alert className='mt-2' >
+                <Alert.Heading>
+                    Quiz Complete!
+                </Alert.Heading>
+                <hr ref={props.scoreAlertRef} />
+                <p>You have correctly choosen <b>{props.score}</b> out of <b>{props.questions.length}</b> answers</p>
+                <p>Meaning, you got <b>{correctPercentage}%</b> of the quiz right!</p>
+            </Alert>
+        </>
     )
-    
+
     return (
         <div style={{ width: '45vw', margin: 'auto auto' }} className="mt-4">
             {
-                props.questions.map(question => <Question
-                    key={nanoid()}
-                    questionData={question}
-                    handleClick={props.handleClick}
-                    disabled={props.quizDisabled}
-                />)
+                props.questions.map(
+                    (question, index) => {
+                        return (
+                            <>
+                                <Question
+                                    key={nanoid()}
+                                    questionData={question}
+                                    handleClick={props.handleClick}
+                                    disabled={props.quizDisabled}
+                                />
+                                {index !== props.questions.length - 1 && <hr />}
+                            </>
+                        )
+                    })
             }
-            <ButtonGroup className='d-flex justify-content-center' >
-                <Button
-                    variant='success'
-                    onClick={props.handleSubmit}
-                    disabled={!props.maySubmit}
-                >Submit Answers</Button>
-                <Button
-                    variant='danger'
-                    onClick={props.handleClear}
-                >Clear Answers</Button>
-            </ButtonGroup>
-            {props.displayAlert.current && scoreAlert}
+            {
+                !props.gameOver.current ?
+                    <ButtonGroup className='d-flex justify-content-center mt-2' >
+                        <Button
+                            variant='success'
+                            onClick={props.handleSubmit}
+                            disabled={!props.maySubmit}
+                        >
+                            Submit Answers
+                        </Button>
+                        <Button
+                            variant='danger'
+                            onClick={props.handleClear}
+                        >
+                            Clear Answers
+                        </Button>
+                    </ButtonGroup> :
+                    <ButtonGroup className='d-flex justify-content-center mt-2' >
+                        <Button
+                            variant='outline-success'
+                            onClick={props.handleSubmit}
+                            disabled={!props.maySubmit}
+                        >
+                            Back To Start
+                        </Button>
+                        <Button
+                            variant='outline-primary'
+                            onClick={props.handleClear}
+                        >
+                            Restart
+                        </Button>
+                    </ButtonGroup>
+
+            }
+            {props.gameOver.current && scoreAlert}
         </div>
     );
 }
